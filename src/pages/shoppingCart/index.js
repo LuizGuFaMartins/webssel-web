7b5a35ee05218921c09beb0d8cfe250cfb7e2fec
Gabriel Pissaia <89611752+GabrielPiss@users.noreply.github.com>
2023-06-17 19:25:23 -0300

    Tela de carrinho com card e barra de pesquisa

--- src/pages/shoppingCart/index.js
+++ src/pages/shoppingCart/index.js
@@ -1,8 +1,77 @@
 import React from "react";
 import "./styles.css";
 
-const ShoppingCart = () => {
-  return <div>ShoppingCart</div>;
-};
-
-export default ShoppingCart;
+  function ShoppingCart() {
+    const [codigoItem, setCodigoItem] = React.useState('');
+    const [produto, setProduto] = React.useState('');
+    const [preco, setPreco] = React.useState('');
+    const [quantidade, setQuantidade] = React.useState('');
+    const [valorTotal, setValorTotal] = React.useState('');
+  
+    const handleCodigoItemChange = (event) => {
+      setCodigoItem(event.target.value);
+    };
+  
+    const handleProdutoChange = (event) => {
+      setProduto(event.target.value);
+    };
+  
+    const handlePrecoChange = (event) => {
+      setPreco(event.target.value);
+    };
+  
+    const handleQuantidadeChange = (event) => {
+      setQuantidade(event.target.value);
+    };
+  
+    const handleValorTotalChange = (event) => {
+      setValorTotal(event.target.value);
+    };
+  
+      return (
+      <div className="container">
+          <div className="search-bar">
+            <input type="text" placeholder="Buscar por nome do produto..."/>
+          </div>
+        <div className="boxcard">    
+            <div className="card">
+              <div className="card-body">
+                <div className="form-groupcod">
+               <label>Código do Item:</label>
+                <span>ola</span>
+                </div>
+                <div className="form-group">
+                  <label>Produto:</label>
+                </div>
+                <div>
+                  <span>ola pessoas</span>
+                </div>
+                <div className="form-group">
+                  <label>Preço:</label>
+                </div>
+                <div>
+                  <span>ola pessoas</span>
+                </div>
+                <div className="form-group">
+                  <label>Quantidade:</label>
+                </div>
+                <div>
+                  <span>ola pessoas</span>
+                </div>
+                <div className="form-group">
+                  <label>Valor total do item:</label>
+                </div>
+                <div>
+                  <span>ola pessoas</span>
+                </div>
+                <div className="remove-button">
+                  <button>Remover do carrinho</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+  };
+  
+  export default ShoppingCart;
