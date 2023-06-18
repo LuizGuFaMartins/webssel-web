b6ba1eedb99789c94fdd542591179da1b3a8b6f0
LuizFarabello <luizgufamartins@gmail.com>
2023-06-17 23:15:59 -0300

    feat: implement product socket

--- src/components/productCard/index.js
+++ src/components/productCard/index.js
@@ -1,24 +1,29 @@
+import axios from "axios";
 import React from "react";
 import "./styles.css";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+  async function deleteProduct() {
+    await axios.delete(`http://localhost:3333/products/${product.productId}`);
+  }
+
   return (
     <div className="card">
       <div className="card-body">
         <div className="form-group">
           <label>Código do produto:</label>
-          <span>ola</span>
+          <span>{product.productCode}</span>
         </div>
         <div className="form-group">
           <label>Produto:</label>
-          <span>ola pessoas</span>
+          <span>{product.productName}</span>
         </div>
         <div className="form-group">
           <label>Preço:</label>
-          <span>ola pessoas</span>
+          <span>{product.productPrice}</span>
         </div>
         <div className="buttons-box">
-          <button>Excluir</button>
+          <button onClick={deleteProduct}>Excluir</button>
           <button>Comprar</button>
         </div>
       </div>
--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,10 +1,25 @@
 import React from "react";
-import ProductCard from "../../components/producCard";
+import { io } from "socket.io-client";
+import ProductCard from "../../components/productCard";
 import "./styles.css";
+
+const socket = io("http://localhost:3333");
 
 const Product = () => {
   const onSearch = (value) => console.log(value);
-  
+  const [products, setProducts] = React.useState([]);
+
+  React.useEffect(() => {
+    setProducts([]);
+    function receiveProducts(prods) {
+      setProducts([...prods]);
+    }
+
+    socket.on("refreshProductsList", (prods) => {
+      receiveProducts(prods);
+    });
+  }, []);
+
   return (
     <div className="container">
       <div className="search-bar">
@@ -12,12 +27,11 @@ const Product = () => {
         <div className="glass"></div>
       </div>
       <div className="boxcard">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {products.map((prod) => (
+          <div key={prod.productId}>
+            <ProductCard product={prod} />
+          </div>
+        ))}
       </div>
     </div>
   );
