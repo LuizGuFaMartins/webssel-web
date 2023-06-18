6023abc1ea4bec8b595b0a0107e38ee7709066e8
Gabriel Pissaia <89611752+GabrielPiss@users.noreply.github.com>
2023-06-17 22:27:50 -0300

    Botao de finalizar pedido

--- src/pages/shoppingCart/index.js
+++ src/pages/shoppingCart/index.js
@@ -17,6 +17,16 @@ function ShoppingCart() {
         <ItemCard />
         <ItemCard />
       </div>
+      <div className="confirmation-card">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Valor total do pedido:</h5>
+            <div className="card-buttons">
+              <button className="btn-buy">Finalizar Pedido</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
